98801f126fb9286a9e99d6a27a0fe1e2de6b907e
W. Matthew Wilson <matt@tplus1.com>
2010-02-19 22:23:53 -0500

    This is matt's contribution

--- snakeplan/urls.py
+++ snakeplan/urls.py
@@ -7,4 +7,53 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # (r'^snakeplan/', include('snakeplan.foo.urls')),
      (r'^admin/', include(admin.site.urls)),
+     (r'^foo$', 'snakeplan.projects.views.foo'),
+     (r'^projects$', 'snakeplan.projects.views.foo'),
 )
+
+
+"""
+====== ======================== ========================================
+Method URL                      What it does
+====== ======================== ========================================
+GET    /modelname/              Lists model instances, using filters
+                                like ?key=value1&key=value2
+
+GET    /modelname/search        displays a search form
+
+GET    /modelname/id            displays a readonly instance
+
+GET    /modelname/edit/id       displays an edit form
+
+POST   /modelname/update/id     updates an instance and redirects
+
+POST   /modelname/upsert        Updates or inserts an instance and
+                                redirects
+
+GET    /modelname/upsert        Draws a form to do an upsert.
+
+GET    /modelname/upsertform    Draws a form to do an upsert.
+
+GET    /modelname/create        displays an insert form
+
+GET    /modelname/insertform    displays an insert form
+
+GET    /modelname/remove/id     displays a form that posts to
+                                /model/delete/id
+
+POST   /modelname/insert        inserts a new record and redirects
+
+POST   /modelname/delete/id     deletes a record and redirects
+
+GET    /modelname/bulk/edit     display a bulk edit ui
+
+POST   /modelname/bulk/update   performs a bulk update and redirect
+
+GET    /modelname/bulk/create   display a bulk insert form
+
+POST   /modelname/bulk/insert   performs a bulk insert and redirect
+
+POST   /modelname/bulk/delete   performs a bulk delete and redirect
+
+====== ======================== ========================================
+"""
