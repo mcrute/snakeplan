e6e1b94472a03b9e9e390fc707ea297963104bd2
Mike Crute <mcrute@gmail.com>
2010-02-20 23:38:47 -0500

    Starting to tidy up the views

--- snakeplan/urls.py
+++ snakeplan/urls.py
@@ -1,3 +1,4 @@
+from django.views import static
 from django.conf.urls.defaults import patterns, include
 
 from django.contrib import admin
@@ -10,4 +11,5 @@ from snakeplan.projects import admin as snakeplan_admin
 urlpatterns = patterns('',
      (r'^admin/', include(admin.site.urls)),
      (r'^project/', include('snakeplan.projects.urls')),
+     (r'^static/(?P<path>.*)$', static.serve, {'document_root':'../static'}),
 )
