1093383ae03816387956b1c21e3799029fda0721
Mike Crute <mcrute@gmail.com>
2010-07-10 23:53:54 -0400

    Getting project add and update working.

--- snakeplan/projects/views/projects.py
+++ snakeplan/projects/views/projects.py
@@ -1,34 +1,49 @@
+# vim: set filencoding=utf8
+"""
+Project Views
+
+@author: Mike Crute (mcrute@gmail.com)
+@organization: SoftGroup Interactive, Inc.
+@date: July 10, 2010
+"""
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from django.views.generic import list_detail, create_update
 from django.core.urlresolvers import reverse
 
-from snakeplan.projects.models import Project
-from snakeplan.projects.models import Iteration
-from snakeplan.projects.forms import ProjectForm
-
+from ..models import Project, Iteration
 
 
 def index(request):
-    return list_detail.object_list(
-        request=request,
-        queryset=Project.objects.order_by('-active', 'name').all(),
-        allow_empty=True
-        )
+    return list_detail.object_list(request,
+        queryset=Project.objects.order_by('-active', 'name').all())
 
 
 def project_iterations(request, project_id):
     project = Project.objects.get(id=project_id)
-    iterations = project.iteration_set.all()
 
-    return list_detail.object_list(
-        request=request,
-        queryset=iterations,
-        extra_context={'project_name': project},
-        allow_empty=True
-        )
+    return list_detail.object_list(request,
+        extra_context={'project_name': project.name },
+        queryset=project.iteration_set.all())
 
 
 def create_project(request):
-    post_save_redirect = '/project/%(id)s/'
-    return create_update.create_object(request,
-            form_class=ProjectForm,
-            post_save_redirect=post_save_redirect)
+    return create_update.create_object(request, model=Project,
+            post_save_redirect=reverse('project-list'))
+
+
+def update_project(request, project_id):
+    return create_update.update_object(request, model=Project,
+            object_id=project_id,
+            post_save_redirect=reverse('project-list'))
