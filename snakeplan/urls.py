d471329e7a5bf7819bb557241bf580a1c6e1fae5
Mike Crute <mcrute@gmail.com>
2010-07-10 23:56:13 -0400

    Tidying up URLs. Still needs some work.

--- snakeplan/urls.py
+++ snakeplan/urls.py
@@ -21,23 +21,12 @@ SnakePlan URL Configuration
 from django.views import static
 from django.conf.urls.defaults import patterns, include
 
-from django.contrib import admin
-admin.autodiscover()
-
-# Just get the admin stuff, don't actually do anything with it
-from snakeplan.projects import admin as _
-from snakeplan.accounts import admin as _
+from django.contrib import admin; admin.autodiscover()
 
 
 urlpatterns = patterns('django.views.generic.simple',
-     (r'^$', 'redirect_to', dict(url='/p/projects/')),
-     (r'^p/project/(?P<id>\d+)/$', 'redirect_to', dict(url='/p/project/%(id)s/iterations/')),
-     (r'^p/iteration/(?P<id>\d+)/$', 'redirect_to', dict(url='/p/iteration/%(id)s/stories/')),
-     (r'^p/story/(?P<id>\d+)/$', 'redirect_to', dict(url='/p/story/%(id)s/tasks/'))
-)
-
-urlpatterns += patterns('',
-     (r'^admin/', include(admin.site.urls)),
-     (r'^p/', include('projects.urls')),
-     (r'^media/(?P<path>.*)$', static.serve, {'document_root':'templates/media'}),
+    (r'^$', 'redirect_to', {'url': 'projects/' }),
+    (r'^projects/', include('projects.urls')),
+    (r'^admin/', include(admin.site.urls)),
+    (r'^media/(?P<path>.*)$', static.serve, {'document_root':'templates/media'}),
 )
